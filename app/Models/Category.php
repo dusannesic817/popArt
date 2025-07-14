14ea2bb0a69fcff630ee9bfd3f4c544e18c6774a
dusannesic817 <dusannesic28@gmail.com>
2025-07-14 14:23:45 +0200

    edit_relatioship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,15 @@ class Category extends Model
 {
     use HasFactory;
 
-        public function products(){
+    public function products(){
         return $this->hasMany(Product::class,'category_id');
+    }
+
+    public function parent()    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children(){
+        return $this->hasMany(Category::class, 'parent_id');
     }
 }
